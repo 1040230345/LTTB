afe44cf28bf8644626eaa90e80c8da4bec2b6f35
AKA-YYY <475668579@qq.com>
2019-10-25 23:08:06 +0800

    “初始提交”

---
+++ src/test/java/com/rainbowbaby/LttbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowbaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LttbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
