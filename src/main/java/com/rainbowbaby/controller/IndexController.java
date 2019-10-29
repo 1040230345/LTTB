74a7b9bc45619b78820083700b2ac5ff1a5609fd
Jiesong.Zhang <javajies@163.com>
2019-10-30 01:07:03 +0800

    音乐搜索播放功能实现，加入缓存

---
+++ src/main/java/com/rainbowbaby/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.rainbowbaby.controller;
+
+import com.rainbowbaby.my_util.GetMusic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 首页控制器
+ */
+@Controller
+public class IndexController {
+    @Autowired
+    private GetMusic getMusic;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @RequestMapping("/post/{musicName}")
+    @ResponseBody
+    public String index(@PathVariable("musicName")String musicName){
+        //先查询缓存中是否存在
+        String st = stringRedisTemplate.opsForValue().get(musicName);
+        if(st!=null){
+            return "playMusic("+"{\"a\":\""+st+"\"})";
+        }
+        st = getMusic.GetMusicByName(musicName);
+        //字符串分割
+        String[] str = st.split("=");
+        //存入缓存
+        stringRedisTemplate.opsForValue().set(musicName,str[1]);
+        st = "playMusic("+"{\"a\":\""+str[1]+"\"})";
+        return st;
+    }
+}
--- src/main/java/com/rainbowbaby/my_util/GetMusic.java
+++ src/main/java/com/rainbowbaby/my_util/GetMusic.java
@@ -1,34 +1,31 @@
-package com.rainbowbaby;
+package com.rainbowbaby.my_util;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.stereotype.Component;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class LttbApplicationTests {
-
-    @Test
-    public void contextLoads() throws InterruptedException {
+/**
+ * 工具类，获取音乐id
+ */
+@Component
+public class GetMusic {
+    public String GetMusicByName(String musicName){
         ChromeOptions options = new ChromeOptions();
         // 设置允许弹框
         //options.addArguments("disable-infobars","disable-web-security");
         // 设置无gui 开发时还是不要加，可以看到浏览器效果
         options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--hide-scrollbars");
         options.addArguments("blink-settings=imagesEnabled=false");
         //String driverPath =  "D:\\crawler-plugin\\chromedriver.exe";
         //System.setProperty("webdriver.chrome.driver", driverPath);
         RemoteWebDriver driver=  new ChromeDriver(options);
-        driver.get("https://music.163.com/#/search/m/?s=安河桥");
+        driver.get("https://music.163.com/#/search/m/?s="+musicName);
         WebElement iframe = driver.findElement(By.tagName("iframe"));
         driver.switchTo().frame(iframe);
         // 然后再去获取元素或者其他操作、操作完需切换回来
@@ -37,7 +34,7 @@ public class LttbApplicationTests {
         String st = driver.findElement(By.className("text")).findElement(By.tagName("a")).getAttribute("href");
         //System.out.println(driver.findElement(By.tagName("a")).getText());
         System.out.println(st);
-
+        driver.close();
+        return st;
     }
-
 }
